0ced871665f43a75aab67a8802c84859efc36775
Sasank Kothe <ga53xil@mytum.de>
2020-02-08 18:00:38 +0100

    trying to ignore map key

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import config from '../config.js'
+import MapConfig from '../mapconfig'
 import GoogleMapReact from "google-map-react";
 
 export default class MapView extends Component {
 
   constructor(props) {
       super(props);
-      
+
     this.state = {
       center: {},
       zoom: 7,
@@ -22,10 +22,9 @@ export default class MapView extends Component {
         <div className="mapView">
         <GoogleMapReact
                        
-                        bootstrapURLKeys={{key: config.GOOGLE_MAPS_API_KEY}}
+                        bootstrapURLKeys={{key: MapConfig.GOOGLE_MAPS_API_KEY}}
                         zoom={this.state.zoom}
                         center={this.state.initialCenter}
-                       
                         >
                        
                     </GoogleMapReact>
