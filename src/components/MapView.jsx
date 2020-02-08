9890967a64edd6cde0d5f10e49113a01d670da1f
Sasank Kothe <ga53xil@mytum.de>
2020-02-08 17:10:33 +0100

    added config file to hold the api key of the map and added map on to the webpage

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,12 +1,38 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import config from '../config.js'
+import GoogleMapReact from "google-map-react";
 
 export default class MapView extends Component {
-    render () {
-        return (
-            <div className="mapView">
-                <h1>Here comes the map</h1>
-            </div>
-        )
-    }
+
+  constructor(props) {
+      super(props);
+      
+    this.state = {
+      center: {},
+      zoom: 7,
+      initialCenter: {
+        lat: 59.95,
+        lng: 30.33
+      }
+    };
+  }
+  render() {
+    return (
+      <>
+        <div className="mapView">
+        <GoogleMapReact
+                       
+                        bootstrapURLKeys={{key: config.GOOGLE_MAPS_API_KEY}}
+                        zoom={this.state.zoom}
+                        center={this.state.initialCenter}
+                       
+                        >
+                       
+                    </GoogleMapReact>
+
+
+        </div>
+      </>
+    );
+  }
 }
---
+++ src/config.js
@@ -0,0 +1,6 @@
+
+let config = {
+    GOOGLE_MAPS_API_KEY: "AIzaSyBT4Vi9cBuTL-3ln90X-K4qaL9Mlcy7jSU",
+};
+
+export default config
