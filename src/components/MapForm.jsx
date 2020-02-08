ebab9435faf8b8d4cc8367c84da802ec370a1a07
Sasank Kothe <ga53xil@mytum.de>
2020-02-08 17:44:12 +0100

    added the form and hid the google maps api key

--- src/components/MapForm.jsx
+++ src/components/MapForm.jsx
@@ -1,12 +1,60 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
+import TimePicker from 'react-time-picker';
 
 export default class MapForm extends Component {
-    render () {
-        return (
-            <div className="mapForm">
-                <h1>This is the Map form view</h1>
-            </div>
-        )
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            
+        }
+
     }
+
+    componentDidMount(){
+        let hours = new Date().getHours();
+        let min = new Date().getMinutes();
+        let secs = new Date().getSeconds();
+        let currentTime  = hours + ":" + min + ":" + secs;
+        this.setState({
+            time : currentTime
+        })
+    }
+  render() {
+    return (
+      <div className="mapForm">
+        <Form>
+          <Form.Group>
+            <Form.Label>Travelling from</Form.Label>
+            <Form.Control type="text" placeholder="Source" />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Travelling to</Form.Label>
+            <Form.Control type="text" placeholder="Destination" />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Free Until</Form.Label>
+            {/* <Form.Control type="text" placeholder="Select the time" /> */}
+            <TimePicker
+                value = {this.state.time}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>How are you travelling</Form.Label>
+            <Form.Control as="select">
+              <option>Walking</option>
+              <option>Bicycling</option>
+              <option>Transit</option>
+              <option>Driving</option>
+            </Form.Control>
+          </Form.Group>
+        </Form>
+        <Button block variant="primary" type="submit">
+          Let's go
+        </Button>
+      </div>
+    );
+  }
 }
